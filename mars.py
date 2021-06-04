dad231cfe35dc31e6b3e757de60e3a1bd1b4e8eb
Alethia Kielbasa <aleth32@gmail.com>
2021-06-04 14:55:58 -0400

    Read in data

--- mars.py
+++ mars.py
@@ -1,2 +1,6 @@
 # This code will analyze data we collect from mars ice caps
 # Author: A Kielbasa, June 2021
+
+# Import pandas and read in data
+import pandas as pd
+data = pd.read_csv('directory/data.csv")
